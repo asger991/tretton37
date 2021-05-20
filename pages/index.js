db60a25ecf3adab4fe1787e8df24e5c72f8d8779
asger991 <sirjazzper@gmail.com>
2021-05-20 21:42:08 +0200

    added custom infinity scroll

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,68 @@
 /* eslint-disable import/no-unresolved */
+import { useRef, useState, useEffect } from "react";
 import styled from "styled-components";
+import usePagination from "utils/usePagination";
 import StyledContainer from "components/Container";
 import StyledCard from "components/Card";
-import Layout from "components/Layout/";
+import Layout from "components/Layout";
 
 const Home = ({ employees }) => {
+  const { next, currentPage, currentData, maxPage } = usePagination(
+    employees,
+    16
+  );
+
+  const currentEmployees = currentData();
+  const [element, setElement] = useState(null);
+  const observer = useRef();
+  const prevY = useRef(0);
+
+  useEffect(() => {
+    observer.current = new IntersectionObserver(
+      (entries) => {
+        const firstEntry = entries[0];
+        const { y } = firstEntry.boundingClientRect;
+
+        if (prevY.current > y) {
+          next();
+        }
+        prevY.current = y;
+      },
+      { threshold: 0 }
+    );
+  }, []);
+
+  useEffect(() => {
+    const currentElement = element;
+    const currentObserver = observer.current;
+
+    if (currentElement) {
+      currentObserver.observe(currentElement);
+    }
+
+    return () => {
+      if (currentElement) {
+        currentObserver.unobserve(currentElement);
+      }
+    };
+  }, [element]);
+
+  console.log(employees);
   return (
     <Layout>
       <StyledHeader>The fellowship of the tretton37</StyledHeader>
       <StyledDiv>PELLE</StyledDiv>
       <StyledContainer>
-        {employees.map((employee) => (
-          <StyledCard key={employee.id} data={employee} />
-        ))}
+        {currentEmployees &&
+          currentEmployees.map((employee) => (
+            <StyledCard key={employee.id} data={employee} />
+          ))}
       </StyledContainer>
+      {currentPage !== maxPage ? (
+        <h1 ref={setElement}>Fetching more Ninjas...</h1>
+      ) : (
+        <h1>You've met all our Ninjas - for now...</h1>
+      )}
     </Layout>
   );
 };
